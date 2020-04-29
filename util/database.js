bee818efac44f846fb483a29916a8375bc82751e
toddk12 <44487727+toddk12@users.noreply.github.com>
2020-04-29 15:25:31 -0600

    luhylih

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DW_PASS, {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
